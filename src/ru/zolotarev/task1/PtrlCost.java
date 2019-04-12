5c85fa42936265499d1e9558215d442352b4329c
Zolotarev Konstantin <sammerseth@yandex.ru>
2019-04-12 18:03:27 +0300

    обновление дз

--- src/ru/zolotarev/task1/PtrlCost.java
+++ src/ru/zolotarev/task1/PtrlCost.java
@@ -1,8 +1,14 @@
 package ru.zolotarev.task1;
 
 public class PtrlCost {
-    public static void main(String[] Args){
-        int ptrl92 = 41;
-        int ptrl95 = 45жz
+    public static void main(String[] args){
+        int cost92 = 41;
+        int cost95 = 45;
+        int costDF = 46;
+        int necessaryAmount = 10;
+
+        System.out.println(necessaryAmount + " liters of 92 is cost " + (cost92*necessaryAmount));
+        System.out.println(necessaryAmount + " liters of 95 is cost " + (cost95*necessaryAmount));
+        System.out.println(necessaryAmount + " liters of DF is cost " + (costDF*necessaryAmount));
     }
 }
--- src/ru/zolotarev/task1/SecondsToHours.java
+++ src/ru/zolotarev/task1/SecondsToHours.java
@@ -1,4 +1,10 @@
 package ru.zolotarev.task1;
 
 public class SecondsToHours {
+    public static void main(String[] args) {
+        int SecondsInHour = 3600;
+        int SecondsToConvert = 7200;
+        System.out.println("There is " + (SecondsToConvert / SecondsInHour ) + " hours in " + SecondsToConvert + " seconds ");
+
+    }
 }
