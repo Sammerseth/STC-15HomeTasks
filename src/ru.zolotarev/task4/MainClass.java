4f6a0720b840a5c7c8566d2d0b1c7547ec6e9b6c
Zolotarev Konstantin <sammerseth@yandex.ru>
2019-04-30 18:01:15 +0300

    Done

--- src/ru.zolotarev/task4/MainClass.java
+++ src/ru.zolotarev/task4/MainClass.java
@@ -1,5 +1,8 @@
 package ru.zolotarev.task4;
 
+import static ru.zolotarev.task4.Firefighter.crewCounter;
+import static ru.zolotarev.task4.Firefighter.readyCrewMembers;
+
 public class MainClass {
     public static void main(String[] args) {
         boolean calling;
@@ -10,29 +13,31 @@ public class MainClass {
             calling = false;
         }
 
+        Firefighter [] Crew = new Firefighter[4];
+
         DisSerOperator dso1 = new DisSerOperator(28,164,52,"female","Ivanova", "private ",
                 "dispath service operator", 29000);
         VehicleDriver vd1 = new VehicleDriver(50,174,90,"male","Petrov","volunteer ",
                 "vehicle driver",12000);
         SquadLeader sl1 = new SquadLeader(30,180,85,"male","Sidorov","captain ",
                 "squad leader", 42000);
-        Firefighter n1 = new Firefighter(26,179,77,"male","Kuznetsov", "sergeant ",
+        Crew[0] = new Firefighter(26,179,77,"male","Kuznetsov", "sergeant ",
                 "squad leader's assistant", 35000);
-        Firefighter n2 = new Firefighter(24,175,70,"male","Popov","private ",
+        Crew[1] = new Firefighter(24,175,70,"male","Popov","private ",
                 "firefighter", 30000);
-        Firefighter n3 = new Firefighter(25, 178,70,"male","Sokolov","private ",
+        Crew[2] = new Firefighter(25, 178,70,"male","Sokolov","private ",
                 "firefighter", 32000);
-        Firefighter n4 = new Firefighter(22,182,75,"male","Novikov","private ",
+        Crew[3] = new Firefighter(22,182,75,"male","Novikov","private ",
                 "firefighter",29000);
-
-
 
         dso1.getAddress(calling);
         vd1.engineStart();
-        n1.sitInCar();
-        n2.sitInCar();
-        n3.sitInCar();
-        n4.sitInCar();
-        sl1.finalCheck();
+
+        for (int i = 0; i<=crewCounter-1; i++ ){
+            Crew[i].sitInCar();
+        }
+
+        sl1.finalCheck(crewCounter, readyCrewMembers);
+
     }
 }
